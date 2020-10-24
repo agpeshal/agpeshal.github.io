f02d11922efd62d06c681b2d32dd6f8b8af6ba2f
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2020-10-24 19:39:56 +0530

    Website is partially completed

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Contact.css'
+
+export const Contact = () => {
+    return (
+        <div className="contact">
+            <h1> Please Connect me on </h1>
+            <div className="contact__icons">
+                <a href="#"><i className="fab fa-facebook"></i></a>
+                <a href="#"><i className="fab fa-twitter"></i></a>
+                <a href="#"><i className="fab fa-instagram"></i></a>
+                <a href="#"><i className="fab fa-linkedin"></i></a>
+            </div>
+        </div>
+
+    )
+}
