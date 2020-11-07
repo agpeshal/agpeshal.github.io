eae115147928b62e8eee7fa9fd0a648ba13c544d
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2020-11-07 08:16:35 +0530

    Updated the Home.js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,9 @@ import './Home.css';
 export const Home = () => {
     return (
         <div className="main">
-            <img className="main__blob" src={require('../assets/blob.svg')}></img>
             <div className="main__intro">
                 <h1>Hi There</h1>
-                <h4>I'm Peshal Agarwal and I'm a Data Scientist</h4>
+                <h4>I'm Peshal Agarwal and I am an aspiring Data Scientist. I am currently studying Statistics at ETH Zurich</h4>
             </div>
             <div className="main__image">
                 <img alt="background" src={require('../assets/peshal_final.png')}></img>
