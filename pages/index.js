f38e47fc5bba93650554b4f817ff02eb7c9f1c48
Peshal Agarwal <peshala48@gmail.com>
2021-04-29 17:48:57 +0200

    add color to links

--- pages/index.js
+++ pages/index.js
@@ -19,26 +19,28 @@ export default function Home() {
             </a>{" "}
             majoring in Statistics with a focus on Machine learning. I recently
             worked on{" "}
-            <a href="https://arxiv.org/abs/2103.14577">
+            <a style={{ color: "blue" }} href="https://arxiv.org/abs/2103.14577">
               unsupervised domain adaptation
             </a>{" "}
-            in <a href="https://vision.ee.ethz.ch/">Computer Vision Lab</a> with{" "}
-            <a href="https://ee.ethz.ch/the-department/faculty/professors/person-detail.OTAyMzM=.TGlzdC80MTEsMTA1ODA0MjU5.html">
+            in <a style={{ color: "blue" }} href="https://vision.ee.ethz.ch/">Computer Vision Lab</a> with{" "}
+            <a style={{ color: "blue" }} href="https://ee.ethz.ch/the-department/faculty/professors/person-detail.OTAyMzM=.TGlzdC80MTEsMTA1ODA0MjU5.html">
               Prof. Luc Van Gool
             </a>{" "}
             for my master thesis. I have interned at IBM Research and Goldman
             Sachs in the past. Previously, I earned a BS and an MS degree in
             Mathematics and Computing from{" "}
-            <a href="https://www.iitk.ac.in/">IIT Kanpur</a> in India where I
+            <a style={{ color: "blue" }} href="https://www.iitk.ac.in/">IIT Kanpur</a> in India where I
             worked on Bayesian Analysis with{" "}
-            <a href="https://scholar.google.co.in/citations?user=yq8z1rIAAAAJ&hl=en&oi=ao">
+            <a style={{ color: "blue" }} href="https://scholar.google.co.in/citations?user=yq8z1rIAAAAJ&hl=en&oi=ao">
               Prof. Debasis Kundu
             </a>
-            . Here is the{" "}
-            <a href="https://scholar.google.co.in/citations?user=TD4121cAAAAJ&hl=en">
-              link
-            </a>{" "}
-            to my Google Scholar profile.
+            . You can also find me on{" "}
+            <a style={{ color: "blue" }} href="https://www.linkedin.com/in/peshal-agarwal/">
+              Linkedin
+            </a> and{" "}
+            <a style={{ color: "blue" }} href="https://scholar.google.co.in/citations?user=TD4121cAAAAJ&hl=en">
+              Google Scholar
+            </a>.
           </p>
           <div className={styles.main__introBtn}>
             {/* <Button
